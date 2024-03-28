874a671e985b1f183ff5eae96282047c7abf702d
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-03-28 22:36:40 +0100

    Setup Tailwind Config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,15 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      height: {
+        screen: "100dvh",
+      },
+      colors: {
+        primary: "#6b8afd",
+        secondary: "#2e333d",
+        dark: "#212328",
+        danger: "#eb3330",
+        success: "#4aac68",
       },
     },
   },
