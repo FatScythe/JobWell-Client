2552a07401f67368896eeed516b63f12c254fe39
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-03-29 22:00:20 +0100

    Feat: Completed hero section of the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
 import { Inter } from "next/font/google";
 import Layout from "./layout";
-import Link from "next/link";
+import { Hero } from "@/components/hero/hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <Layout>
-      <main className={`${inter.className}`}>
-        <h1 className='text-red-400'>Home Page</h1>
-      </main>
+      <Hero />
     </Layout>
   );
 }
