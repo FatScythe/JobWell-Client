f20c3979d3bd0dddaab7946ceddcaf82816cd7f4
Abdullahi Fahm Oladayo <holadhayo28@gmail.com>
2024-03-31 17:30:30 +0100

    Added Title and meta to homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
+// Components
 import Layout from "./layout";
-import { Hero } from "@/components/hero/hero";
+import { Hero } from "@/components/home/hero";
+import CTA from "@/components/home/cta";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <Layout>
+      <Head>
+        <title>JobWell</title>
+        <meta
+          property='title'
+          content='JobWell connects employer to employees'
+          key='title'
+        />
+      </Head>
       <Hero />
+      <CTA />
     </Layout>
   );
 }
